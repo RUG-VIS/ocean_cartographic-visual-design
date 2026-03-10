0af9529c98fca6d02b9b8a6afcebaa7b216660f3
Brad Robertson <82551837+WhisperingSpoon@users.noreply.github.com>
2026-03-10 06:51:19 +0100

    Fixed issue with maxint interpolation and capped divergence - reverted to cubic interpolation

--- src/GenerateDesignSpace.py
+++ src/GenerateDesignSpace.py
@@ -1,7 +1,6 @@
 import os
 import itertools
 import traceback
-
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
@@ -37,18 +36,17 @@ skip_title_plotting = True
 # General Parameters
 # -----------------------------------------------------------------------------
 parchment_file = "parchment_v5.png"  # "parchment_v1.jpg"
-bathytopograhy_dir = "/media/christian/MyPassport/data/hydrodynamic/ENWS/"
+bathytopograhy_dir = "large-area-files"
 bathytopograhy_file = "gebco_2025_n59.03_s57.65_w-7.65_e-6.05.nc"   # Path to input elevation data; input_netcdf
 # input_netcdf2 = "metoffice_foam1_amm7_NWS_SSC_hi20220101.nc"   # Path to input flow data
-currents_dir = "/media/christian/MyPassport/data/hydrodynamic/ENWS/reanalysis2D-2024/currents/"
+currents_dir = "large-area-files"
 currents_file = "metoffice_foam1_amm7_NWS_CUR_b20240103_dm20240101.nc"  # input_netcdf2
 # input_netcdf3 = "ThesisTemps.nc"   # Path to input  temperature data
-temperature_dir = "/media/christian/MyPassport/data/hydrodynamic/ENWS/reanalysis2D-2024/temperature/"
+temperature_dir = "large-area-files"
 temperature_file = "metoffice_foam1_amm7_NWS_TEM_b20240103_dm20240101.nc"  # input_netcdf3
-output_dir = "/media/christian/My Passport/Documents/Papers/ISPRS2026/revision_selection"
-# output_dir = "/media/christian/My Passport/Documents/Papers/ISPRS2026/revision_flowhatches"
-# output_dir = "/media/christian/My Passport/Documents/Papers/ISPRS2026/revision_stipples"
+output_dir = "surveytest"  # Directory to save output images
 # output_dir = "/media/christian/My Passport/Documents/Papers/ISPRS2026/revision_flowcontours"
+# output_dir = "/media/christian/My Passport/Documents/Papers/ISPRS2026/revision_stipples"
 
 def round_to_nice(x):
     magnitude = 10 ** np.floor(np.log10(x))
@@ -1220,7 +1218,7 @@ def hatch(attribute1, attribute1_max=None, attribute1_min=None, attribute2=None,
                         aspect='auto', alpha=1.0, zorder=0)
     except FileNotFoundError as error:
         print("Parchment texture file not found. Proceeding without it.")
-        # traceback.format_exc()
+        traceback.format_exc()
         logger.exception(error)
     
     # variables for colorbar control
@@ -1718,7 +1716,7 @@ def stipple(lon, lat, attribute1, attribute2=None, attribute3=None, signed_dist=
                 aspect='auto', alpha=1.0, zorder=0)
     except FileNotFoundError as error:
         print("Parchment texture file not found. Proceeding without it.")
-        # traceback.format_exc()
+        traceback.format_exc()
         logger.exception(error)
 
     if attribute3 is not None:
@@ -2264,7 +2262,7 @@ def flow(attribute1, lon, lat, attribute2, attribute3=None, attribute4=None, sig
                     aspect='auto', alpha=1.0, zorder=0)
         except FileNotFoundError as error:
             print(f"Parchment texture file {parchment_file} not found. Proceeding without it.")
-            # traceback.format_exc()
+            traceback.format_exc()
             logger.exception(error)
 
     # Generate contours based on attribute1
@@ -2282,7 +2280,7 @@ def flow(attribute1, lon, lat, attribute2, attribute3=None, attribute4=None, sig
         print(f"Error generating contours: {e}")
         print(f"Contour levels: {contour_levels}")
         print(f"Data shape: {attribute1.shape}, range: [{min_attr1:.6f}, {max_attr1:.6f}]")
-        # traceback.format_exc()
+        traceback.format_exc()
         logger.exception(e)
         return None
     
@@ -2445,7 +2443,7 @@ def flow(attribute1, lon, lat, attribute2, attribute3=None, attribute4=None, sig
                             if not np.isnan(val):
                                 attr4_vals.append(val)
                         except:
-                            # traceback.format_exc()
+                            traceback.format_exc()
                             pass
                     
                     # Calculate linewidth based on attribute4
@@ -2742,7 +2740,10 @@ def load_sample_data():
     # u = np.squeeze(ds2["uo"].data[0])
     u = np.squeeze(ds2["uo"].data)[0]
     u_attrs = ds2["uo"].attrs
+    fill_value = u_attrs.get("_FillValue", -32768)  # fallback to -32768 if not explicitly set
+    u = np.where(u == fill_value, np.nan, u).astype(np.float32)  # replace with nan before scaling
     # print("u-attrs: {}".format(u_attrs))
+    #print(f"uvel after loading: [{u.min():.4f}, {u.max():.4f}]")
     if "scale_factor" in u_attrs.keys():
         scalefactor = u_attrs["scale_factor"]
         print("U has a scale-factor of {}. Rescaling values ...".format(scalefactor))
@@ -2756,6 +2757,8 @@ def load_sample_data():
     # print(u.shape)
     v = np.squeeze(ds2["vo"].data)[0]
     v_attrs = ds2["vo"].attrs
+    fill_value = v_attrs.get("_FillValue", -32768)  # fallback to -32768 if not explicitly set
+    v = np.where(v == fill_value, np.nan, v).astype(np.float32)  # replace with nan before scaling
     # print("v-attrs: {}".format(v_attrs))
     if "scale_factor" in v_attrs.keys():
         scalefactor = v_attrs["scale_factor"]
@@ -2770,7 +2773,9 @@ def load_sample_data():
     in_lat = ds2["latitude"].data
     in_lon = ds2["longitude"].data
     # print("in_lat {}; in_lon {}".format(in_lat.shape, in_lon.shape))
-    
+    print(f"uvel after scaling: [{u.min():.4f}, {u.max():.4f}]")
+    print(f"vvel after scaling: [{v.min():.4f}, {v.max():.4f}]")
+
     # Load dataset for temperature data
     ds3 = xr.open_dataset(os.path.join(temperature_dir, temperature_file))
     # temp = ds3["thetao"].isel(time=0)  # Temperature (°C)
@@ -2797,23 +2802,24 @@ def load_sample_data():
     mesh_lat, mesh_lon = np.meshgrid(lat, lon, sparse=False, indexing='ij')
     # == elevation_ij = (mesh_lat.flatten(), mesh_lon.flatten()) == #
     # u = u.interp(latitude=lat, longitude=lon, method="linear")  # method="cubic"
-    interp_u = RegularGridInterpolator((in_lat, in_lon), u, bounds_error=False, fill_value=.0)
+    interp_u = RegularGridInterpolator((in_lat, in_lon), u, bounds_error=False, fill_value=.0, method= 'cubic')
     u = interp_u((mesh_lat, mesh_lon))  # method="cubic"
     # v = v.interp(latitude=lat, longitude=lon, method="linear")  # method="cubic"
-    interp_v = RegularGridInterpolator((in_lat, in_lon), v, bounds_error=False, fill_value=.0)
+    interp_v = RegularGridInterpolator((in_lat, in_lon), v, bounds_error=False, fill_value=.0, method= 'cubic')
     v = interp_v((mesh_lat, mesh_lon))  # method="cubic"
     # temp = temp.interp(latitude=lat, longitude=lon, method="linear")  # method="cubic"
-    interp_temp = RegularGridInterpolator((in_lat, in_lon), temp, bounds_error=False, fill_value=.0)
+    interp_temp = RegularGridInterpolator((in_lat, in_lon), temp, bounds_error=False, fill_value=.0, method= 'cubic')
     temp = interp_temp((mesh_lat, mesh_lon))  # method="cubic"
 
     # TODO: again: why are we clamping the fields to a squared area ?
     u = u[:min_size, :min_size]
     v = v[:min_size, :min_size]
     temp = temp[:min_size, :min_size]
-
-    u = np.where(water_mask, u, .0)  # u.where(water_mask)
-    v = np.where(water_mask, v, .0)  # v.where(water_mask)
-    temp = np.where(water_mask, temp, .0)  # temp.where(water_mask)
+    
+    #make v and u zero on land
+    u = np.where(water_mask, u, .0)
+    v = np.where(water_mask, v, .0)
+    temp = np.where(water_mask, temp, .0)
 
     # Compute the magnitude of the flow field
     velmag = u**2 + v**2
@@ -2821,7 +2827,6 @@ def load_sample_data():
     velmag_invalid = np.isclose(velmag, .0) & np.isclose(velmag, -.0)
     velmag_min = np.finfo(velmag.dtype).eps if velmag_invalid.all() else np.nanmin(velmag[~velmag_invalid])
     velmag[velmag_invalid] = velmag_min
-
 
     # Calculate flow divergence
     lon_spacing = float(np.abs(lon[1] - lon[0]))
@@ -2842,8 +2847,7 @@ def load_sample_data():
     
     # Compute divergence
     flow_divergence = dudx + dvdy
-    flow_divergence = np.sign(flow_divergence)  # only use the sign, not the absolute value of divergence
-    
+    flow_divergence = np.maximum(-1.0, np.minimum(1.0, flow_divergence))    
     # Create divergence dataarray
     # divergence = xr.DataArray(
     #     data=flow_divergence,
@@ -2853,7 +2857,7 @@ def load_sample_data():
     
     # Mask out non-water areas in the divergence field
     # divergence = divergence.where(water_mask)
-    flow_divergence = np.where(water_mask, flow_divergence, np.NaN)
+    flow_divergence = np.where(water_mask, flow_divergence, np.nan)
     
     # Replace NaNs and handle infinite values
     flow_divergence = np.nan_to_num(flow_divergence, nan=0, posinf=0, neginf=0)
@@ -2971,14 +2975,14 @@ def generate_all_possible_mappings(data, output_dir="visualisation_outputs"):
     print("Generating visualisations for all possible mappings...")
     
     # ==== Generate all combinations for hatch visualisations ==== #
-    # generate_all_hatch_combinations(data, variables, flow_direction, output_dir)
-    generate_survey_hatch_combinations(data, variables, flow_direction, output_dir)
+    #generate_all_hatch_combinations(data, variables, flow_direction, output_dir)
+    #generate_survey_hatch_combinations(data, variables, flow_direction, output_dir)
     
     # ==== Generate all combinations for stipple visualisations ==== #
-    # generate_all_stipple_combinations(data, variables, output_dir)
+    generate_all_stipple_combinations(data, variables, output_dir)
     
     # ==== Generate all combinations for flow contour visualisations ==== #
-    # generate_all_flow_combinations(data, variables, output_dir)
+    generate_all_flow_combinations(data, variables, output_dir)
     
     print(f"All possible mappings generated and saved to {output_dir}")
 
@@ -3030,8 +3034,8 @@ def generate_all_hatch_combinations(data, variables, flow_direction, output_dir)
         density_var, length_var, color_var, width_var = combo  # Unpack all four variables
         
         # Skip some combinations to make the total number manageable
-        if i % 10 != 0 and len(valid_combinations) > 50:
-            continue
+        #if i % 10 != 0 and len(valid_combinations) > 50:
+        #    continue
             
         # Create descriptive filename components
         density_part = f"{variables[density_var]}-Density"
@@ -3194,7 +3198,7 @@ def generate_all_hatch_combinations(data, variables, flow_direction, output_dir)
             )  # TODO: to be modfied by plotting title
         except Exception as e:
             print(f"Error generating {filename}: {e}")
-            # traceback.format_exc()
+            traceback.format_exc()
             logger.exception(e)
 
 def generate_survey_hatch_combinations(data, variables, flow_direction, output_dir):
@@ -3245,8 +3249,8 @@ def generate_survey_hatch_combinations(data, variables, flow_direction, output_d
         density_var, length_var, color_var, width_var = combo  # Unpack all four variables
 
         # Skip some combinations to make the total number manageable
-        if i % 10 != 0 and len(valid_combinations) > 50:
-            continue
+        #if i % 10 != 0 and len(valid_combinations) > 50:
+        #    continue
 
         # Create descriptive filename components
         density_part = f"{variables[density_var]}-Density"
@@ -3409,7 +3413,7 @@ def generate_survey_hatch_combinations(data, variables, flow_direction, output_d
             )  # TODO: to be modfied by plotting title
         except Exception as e:
             print(f"Error generating {filename}: {e}")
-            # traceback.format_exc()
+            traceback.format_exc()
             logger.exception(e)
     
 
@@ -3454,8 +3458,8 @@ def generate_all_stipple_combinations(data, variables, output_dir):
         contour_var, density_var, background_var = combo
        
         # Skip some combinations to make the total number manageable
-        if i % 2 != 0 and len(valid_combinations) > 20:
-            continue
+        #if i % 2 != 0 and len(valid_combinations) > 20:
+        #    continue
            
         # Create descriptive filename
         filename = f"Stipple_{variables[contour_var]}-Contour_{variables[density_var]}-Density"
@@ -3535,7 +3539,7 @@ def generate_all_stipple_combinations(data, variables, output_dir):
             )
         except Exception as e:
             print(f"Error generating {filename}: {e}")
-            # traceback.format_exc()
+            traceback.format_exc()
             logger.exception(e)
 
 def generate_all_flow_combinations(data, variables, output_dir):
@@ -3582,8 +3586,8 @@ def generate_all_flow_combinations(data, variables, output_dir):
         contour_var, dash_var, background_var, width_var = combo
         
         # Skip some combinations to make the total number manageable
-        if i % 4 != 0 and len(valid_combinations) > 40:
-            continue
+        #if i % 4 != 0 and len(valid_combinations) > 40:
+        #   continue
         
         # Create filename components
         contour_part = f"{variables[contour_var]}-ContourLevels"
@@ -3679,7 +3683,7 @@ def generate_all_flow_combinations(data, variables, output_dir):
             )
         except Exception as e:
             print(f"Error generating {filename}: {e}")
-            # traceback.format_exc()
+            traceback.format_exc()
             logger.exception(e)
 
 def main():
